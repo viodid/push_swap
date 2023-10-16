81ed06b2b06cd44cd2c84e963a88798e6201ee05
viodid <david.zwsse@simplelogin.co>
2023-10-16 21:16:21 +0200

    phase2

--- src/phase1.c
+++ src/phase1.c
@@ -6,7 +6,7 @@
 /*   By: dyunta <dyunta@student.42madrid.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/11 18:07:16 by dyunta            #+#    #+#             */
-/*   Updated: 2023/10/11 19:09:20 by dyunta           ###   ########.fr       */
+/*   Updated: 2023/10/16 21:14:58 by dyunta           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -43,6 +43,48 @@ int	phase1(t_stack *stack_A, t_stack *stack_B)
 	}
 	return (0);
 }
+
+int	phase2(t_stack *stack_A, t_stack *stack_B)
+{
+	int	top1_n;
+	int top2_n;
+
+	if (!check_if_reverse_sorted(stack_A))
+		while (stack_B->top1 >= 0)
+			push(stack_B, stack_A, "pa");
+	while (stack_B->top1 != -1)
+	{
+		top1_n = stack_B->p[stack_B->top1];
+		top2_n = stack_B->p[stack_B->top2];
+		if (top1_n > top2_n)
+		{
+			swap(stack_B, "sb");
+			top1_n = stack_B->p[stack_B->top1];
+		}
+		if (top1_n > stack_B->p[0])
+			rotate(stack_B, "rb");
+		push(stack_B, stack_A, "pa");
+	}
+	return (0);
+}
+
+int check_if_reverse_sorted(t_stack *stack)
+{
+	int i;
+	int last_n;
+	int current_n;
+
+	i = 0;
+	last_n = stack->p[0];
+	while (i++ <= stack->top1)
+	{
+		current_n = stack->p[i++];
+		if (last_n > current_n)
+			return (1);
+	}
+	return (0);
+}
+
 
 int check_if_sorted(t_stack *stack)
 {
