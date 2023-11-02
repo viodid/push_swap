1185777c28822b057872656736335162b8225891
viodid <david.zwsse@simplelogin.co>
2023-11-02 21:43:11 +0100

    argc bug

--- lib/count_args.c
+++ lib/count_args.c
@@ -6,26 +6,21 @@
 /*   By: dyunta <dyunta@student.42madrid.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/23 21:21:34 by dyunta            #+#    #+#             */
-/*   Updated: 2023/11/02 21:27:51 by dyunta           ###   ########.fr       */
+/*   Updated: 2023/11/02 21:36:55 by dyunta           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/libft.h"
 
+/*
+ * Counts the total number of args in **argv.
+ */
 int	count_args(char **argv)
 {
 	int	i;
 
 	i = 0;
 	while (argv[i])
-	{
-		ft_strtol(argv[i]);
 		i++;
-	}
-	if (errno)
-	{
-		i--;
-		errno = 0;
-	}
 	return (i);
 }
